336fc734d9df81e8dc36e5dd1d9b53a802c6e7b5
Dmitriy <dimnissv@yandex.kz>
2026-04-30 10:37:39 +0500

    feat: use postgres

--- core/settings.py
+++ core/settings.py
@@ -90,11 +90,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'psql': env.db('DATABASE_URL', default='postgres://postgres:postgres@localhost:5432/just_content_platform')
+    'default': env.db('DATABASE_URL', default='sqlite:///db.sqlite3')
 }
 
 # Password validation
