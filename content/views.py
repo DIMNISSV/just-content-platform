863c4451d8049c5252ea41a7b30137c2f56ab09a
Dmitriy <dimnissv@yandex.kz>
2026-07-14 15:47:53 +0500

    feat(content): enqueue_title_refresh in views

--- content/views.py
+++ content/views.py
@@ -12,7 +12,7 @@ from rest_framework.decorators import api_view, action, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from rest_framework.response import Response
 
-from aggregator.tasks import trigger_title_refresh_task
+from aggregator.tasks import enqueue_title_refresh
 from media.models import Asset, RawMediaFile, AssetVariant, TranscodingPreset
 from media.tasks import extract_stream_task
 from users.models import UserPreference
@@ -385,7 +385,7 @@ class WatchView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        trigger_title_refresh_task.delay(str(self.object.id))
+        enqueue_title_refresh(self.object)
 
         context['content_type'] = 'title'
         context['player_id'] = self.object.id
@@ -471,7 +471,7 @@ class EpisodeWatchView(DetailView):
         context = super().get_context_data(**kwargs)
         title = self.object.title
 
-        trigger_title_refresh_task.delay(str(title.id))
+        enqueue_title_refresh(title)
 
         context['title'] = title
         context['content_type'] = 'episode'
