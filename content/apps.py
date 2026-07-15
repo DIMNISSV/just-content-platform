c7542dc800114e53f462ed3018ec576cfb8628cd
Dmitriy <dimnissv@yandex.kz>
2026-07-15 11:05:09 +0500

    fix(content): removed fix for sqlite

--- content/apps.py
+++ content/apps.py
@@ -6,15 +6,3 @@ class ContentConfig(AppConfig):
 
     def ready(self):
         import content.signals
-
-        from django.db.models.fields import Field
-        from content.lookups import (
-            PostgresILIKEIContains,
-            PostgresILIKEIStartsWith,
-            PostgresILIKEIEndsWith,
-            PostgresILIKEIExact
-        )
-        Field.register_lookup(PostgresILIKEIContains)
-        Field.register_lookup(PostgresILIKEIStartsWith)
-        Field.register_lookup(PostgresILIKEIEndsWith)
-        Field.register_lookup(PostgresILIKEIExact)
